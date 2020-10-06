fae767b2dcc74bcfc1ee12639726ff3cb19c2d94
fyeee <fengkai.ye@mail.utoronto.ca>
2020-10-06 12:58:38 -0400

    now identifies more than one company in reports need to migrate to pdf cleaning process

--- CompanyNameRecognition.py
+++ CompanyNameRecognition.py
@@ -3,6 +3,7 @@ from nltk.tree import Tree
 from fuzzywuzzy import fuzz
 import pandas as pd
 import time
+import os
 
 
 def get_continuous_chunks(text):
@@ -38,18 +39,20 @@ def identify_proper_noun(text):
     return nouns
 
 
-def identify_company_name(company_names_list, all_possible_name_list):
+def identify_company_name(company_names_list, all_possible_name_list, target_company):
     name_list = []
-    global target_company
     for possible_name in all_possible_name_list:
-        if target_company != "":
-            if possible_name not in name_list and fuzz.token_set_ratio(target_company, possible_name) > 90:
+        # if target_company != "":
+        #     if possible_name not in name_list and fuzz.token_set_ratio(target_company, possible_name) > 90:
+        #         name_list.append(possible_name)
+        # else:
+        if possible_name not in name_list and fuzz.token_set_ratio(target_company, possible_name) > 90:
+            name_list.append(possible_name)
+            continue
+        for company in company_names_list:
+            if possible_name not in name_list and fuzz.ratio(possible_name.lower(), company.lower()) > 85:
+                # target_company = company
                 name_list.append(possible_name)
-        else:
-            for company in company_names_list:
-                if possible_name not in name_list and fuzz.ratio(possible_name.lower(), company.lower()) > 80:
-                    target_company = company
-                    name_list.append(possible_name)
     return name_list
 
 
@@ -59,23 +62,27 @@ def name_removal(line, list_of_names):
         line = line.replace(name,'')
     return line
 
-target_company = ""
+
 apple_path = r"TestingFiles\2018-01-17-AAPL.OQ-Apple Inc-BofA Global Research-Apple Inc. What’s the playbook for 2018- A bull a...-80603721.txt"
 apple2_path = r"TestingFiles\2018-01-26-AAPL.OQ-Apple Inc-BTIG-Apple, Inc. - How Bad Will Apples Guidance Be-80704228.txt"
 tsco_path = r"TestingFiles\2018-01-11-TSCO.OQ-Tractor Su-Wedbush Securities I-Downgrading to NEUTRAL; Key Drivers Improving, but...-80541238.txt"
+target_path = tsco_path
 if __name__ == "__main__":
     start = time.time()
+    report_name = os.path.splitext(target_path)[0]
+    company_ticker = report_name.split("-")[3]
     company_df = pd.read_excel("./list_SP500.xlsx", sheet_name="S&P500")
 
+    company_df["S&P 500 Index"] = company_df["S&P 500 Index"].str.slice(0, -4)
     company_names = company_df["S&P 500 Index"]
-    company_names = company_names.str.slice(0, -4)
-    with open(tsco_path, 'rb') as file:
+    target_company = company_df[company_df[".SP500"] == company_ticker]["S&P 500 Index"].max()
+    with open(target_path, 'rb') as file:
         count = 0
         line = file.readline().decode('utf-8')
         while line:
             all_possible_names = get_continuous_chunks(line)
             all_possible_names.extend(identify_proper_noun(line))
-            recognized_names = identify_company_name(company_names, all_possible_names)
+            recognized_names = identify_company_name(company_names, all_possible_names, target_company)
             if len(recognized_names) > 0:
                 print(recognized_names)
                 print(line)
